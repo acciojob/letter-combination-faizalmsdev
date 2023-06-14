68818c13a1d5561966fd422d138d1b9fa489d887
MOHAMED FAIZAL M.S <108245146+faizalmsdev@users.noreply.github.com>
2023-06-14 20:23:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,44 @@
 function letterCombinations(input_digit) {
-  //Complete the function
+  // Mapping of digits to letters
+  const digitToLetters = {
+    2: "abc",
+    3: "def",
+    4: "ghi",
+    5: "jkl",
+    6: "mno",
+    7: "pqrs",
+    8: "tuv",
+    9: "wxyz",
+  };
+
+  const combinations = [];
+
+  // Recursive function to generate letter combinations
+  function generateCombinations(index, currentCombination) {
+    if (index === input_digit.length) {
+      // All digits have been processed, add the current combination to the result
+      combinations.push(currentCombination);
+      return;
+    }
+
+    const digit = input_digit[index];
+    const letters = digitToLetters[digit];
+
+    // Iterate through each letter of the current digit
+    for (let i = 0; i < letters.length; i++) {
+      const letter = letters[i];
+      // Recursively generate combinations with the next digit
+      generateCombinations(index + 1, currentCombination + letter);
+    }
+  }
+
+  // Start the recursive function with an empty initial combination
+  generateCombinations(0, "");
+
+  // Sort the combinations lexicographically
+  combinations.sort();
+
+  return combinations;
 }
 
 module.exports = letterCombinations;
